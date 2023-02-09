e0df15fd97c9f08e9c85acb808f1b1d2cffc839e
Ophelie <opheliewask@yahoo.fr>
2023-02-09 17:43:28 +0100

    modification de login et de home & ajout de la page home

--- components/Home.js
+++ components/Home.js
@@ -1,40 +1,58 @@
 import styles from '../styles/Home.module.css';
-
+import Image from 'next/image';
 function Home() {
-  return (
-    <div className={styles.home}>
-        <div className={styles.left}>
-                <span className={styles.logo}>
-                <img src="logo.webtwitter.webp" alt="logo" className={styles.img}/>
-                </span>
-        </div>
-        <div className={styles.right}>
-            <div className={styles.logo}>
-                <span className={styles.logoRight}>
-                    <img src="logo.webtwitter.webp" alt="logo" className={styles.imgRight}/>
-                </span>
+    return (
+      <div className={styles.PageTwitter}>
+            <div className={styles.left}>
+                    <div className={styles.logo}>
+                            <Image src="/logo.webp" 
+                            alt="logo" 
+                            className={styles.imgLeft}
+                            width={50}
+                            height={50}/>
+                    </div>
+                    <div className={styles.bottomLeft}>
+                        <div className={styles.id}>
+                            <div className={styles.user}>
+                                <Image 
+                                src="/user.webp" 
+                                alt="user image" 
+                                className={styles.userimg}
+                                width={50}
+                                height={50}
+                                />
+                            </div>
+                              {/* user */}
+                        </div>
+                        <div className={styles.logout_btn}>
+                            <button className={styles.logout}>logout</button>
+                        </div>
+                    </div>
             </div>
-            <div className={styles.title}>
-                <h2>
-                  See what's
-                  Happening
-                </h2>
+            <div className={styles.center}>
+                    <div className={styles.writtentweet}>
+                        <div className={styles.titleHome}>
+                            <span>Home</span>
+                        </div>
+                        <div className={styles.addTweet}>
+                            {/* add tweet */}
+                        </div>
+                    </div>
+                    <div className={styles.tweets}>
+                        {/* tweets */}
+                    </div>
             </div>
-            <div className={styles.sub_title}>
-                <h3>Join Hackatweet today.</h3>
-            </div>
-            <div className={styles.signing}>
-                <div className={styles.sign_up}>
-                    <a className={styles.sign_upBtn}>Sign up</a>
+            <div className={styles.right}>
+                <div className={styles.Trends}>
+                    <span>Trends</span>
                 </div>
-                <span className={styles.question}>Already have an account?</span>
-                <div className={styles.signin}>
-                    <a className={styles.signinBtn}>Signin</a>
+                <div>
+                    {/* # */}
                 </div>
             </div>
-        </div>
-    </div>
-  );
-}
-
-export default Home;
+      </div>
+    );
+  }
+  
+  export default Home;
+  
---
+++ components/Login.js
@@ -0,0 +1,40 @@
+import styles from '../styles/Login.module.css';
+
+function Login() {
+  return (
+    <div className={styles.home}>
+        <div className={styles.left}>
+                <span className={styles.logo}>
+                <img src="logo.webtwitter.webp" alt="logo" className={styles.img}/>
+                </span>
+        </div>
+        <div className={styles.right}>
+            <div className={styles.logo}>
+                <span className={styles.logoRight}>
+                    <img src="logo.webtwitter.webp" alt="logo" className={styles.imgRight}/>
+                </span>
+            </div>
+            <div className={styles.title}>
+                <h2>
+                  See what's
+                  Happening
+                </h2>
+            </div>
+            <div className={styles.sub_title}>
+                <h3>Join Hackatweet today.</h3>
+            </div>
+            <div className={styles.signing}>
+                <div className={styles.sign_up}>
+                    <a className={styles.sign_upBtn}>Sign up</a>
+                </div>
+                <span className={styles.question}>Already have an account?</span>
+                <div className={styles.signin}>
+                    <a className={styles.signinBtn}>Signin</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Login;
